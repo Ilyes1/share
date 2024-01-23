9c8b637a765ae5222da337db3b42c63fc485ad1c
Ilyes1 <ihannouch7@gmail.com>
2024-01-23 12:23:19 +0100

    changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
 const screenshot = require('desktop-screenshot');
-const schedule = require('node-schedule');
+const fs = require('fs');
 
 const app = express();
 const server = http.createServer(app);
@@ -10,45 +10,30 @@ const io = socketIo(server);
 
 app.use(express.static('public'));
 
-let screenshotJob;
+let screenshotPath = 'screenshot.png';
 
 io.on('connection', (socket) => {
     console.log('User connected');
 
-    socket.on('start-share', () => {
-        // Schedule to take screenshots every second
-        screenshotJob = schedule.scheduleJob('* * * * * *', async () => {
-            const screenshotData = await takeScreenshot();
-            io.emit('screenshot', screenshotData);
-        });
-    });
-
-    socket.on('stop-share', () => {
-        // Stop taking screenshots when requested
-        if (screenshotJob) {
-            screenshotJob.cancel();
-        }
+    socket.on('start-share', async () => {
+        const screenshotData = await takeScreenshot();
+        io.emit('screenshot', screenshotData);
     });
 
     socket.on('disconnect', () => {
         console.log('User disconnected');
-        // Stop taking screenshots when a user disconnects
-        if (screenshotJob) {
-            screenshotJob.cancel();
-        }
     });
 });
 
 async function takeScreenshot() {
     return new Promise((resolve, reject) => {
-        screenshot("screenshot.png", function(error, complete) {
+        screenshot(screenshotPath, function(error, complete) {
             if (error) {
                 console.error('Error taking screenshot:', error);
                 reject(error);
             } else {
                 // Read the screenshot file and emit it as base64 data
-                const fs = require('fs');
-                const screenshotData = fs.readFileSync("screenshot.png", { encoding: 'base64' });
+                const screenshotData = fs.readFileSync(screenshotPath, { encoding: 'base64' });
                 resolve(screenshotData);
             }
         });
