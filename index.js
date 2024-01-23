75ec08409a32f2668be5f36a9155df6ab156c804
Ilyes1 <ihannouch7@gmail.com>
2024-01-23 12:05:22 +0100

    changes

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const screenshot = require('desktop-screenshot');
+const schedule = require('node-schedule');
 
 const app = express();
 const server = http.createServer(app);
@@ -8,25 +10,50 @@ const io = socketIo(server);
 
 app.use(express.static('public'));
 
+let screenshotJob;
+
 io.on('connection', (socket) => {
     console.log('User connected');
 
+    socket.on('start-share', () => {
+        // Schedule to take screenshots every second
+        screenshotJob = schedule.scheduleJob('* * * * * *', async () => {
+            const screenshotData = await takeScreenshot();
+            io.emit('screenshot', screenshotData);
+        });
+    });
+
+    socket.on('stop-share', () => {
+        // Stop taking screenshots when requested
+        if (screenshotJob) {
+            screenshotJob.cancel();
+        }
+    });
+
     socket.on('disconnect', () => {
         console.log('User disconnected');
-    });
-
-    socket.on('offer', (offer) => {
-        socket.broadcast.emit('offer', offer);
-    });
-
-    socket.on('answer', (answer) => {
-        socket.broadcast.emit('answer', answer);
-    });
-
-    socket.on('ice-candidate', (candidate) => {
-        socket.broadcast.emit('ice-candidate', candidate);
+        // Stop taking screenshots when a user disconnects
+        if (screenshotJob) {
+            screenshotJob.cancel();
+        }
     });
 });
+
+async function takeScreenshot() {
+    return new Promise((resolve, reject) => {
+        screenshot("screenshot.png", function(error, complete) {
+            if (error) {
+                console.error('Error taking screenshot:', error);
+                reject(error);
+            } else {
+                // Read the screenshot file and emit it as base64 data
+                const fs = require('fs');
+                const screenshotData = fs.readFileSync("screenshot.png", { encoding: 'base64' });
+                resolve(screenshotData);
+            }
+        });
+    });
+}
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
